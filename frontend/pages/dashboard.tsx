cfbe4c5863f6dfb0652cca0f5bb309ce3fee94e3
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-04 13:03:55 -0300

    Adiconando api

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../context/AuthContext"
+import { api } from "../services/api";
+
+
+export default function Dashboard() {
+    const { user } = useContext(AuthContext);
+    useEffect(() => {
+        api.get('/me').then(response => console.log(response))
+    }, [])
+
+    return(
+        <h1>DashBoard: {user?.email}</h1>
+    )
+} 
